5af0f31f4ea452870e4f56e1d398e1a8129143de
Joel Kurian Biju <92435259+Joelkb@users.noreply.github.com>
2023-01-09 22:18:07 +0530

    Delete route.py

--- route.py
+++
@@ -1,9 +0,0 @@
-#For Paid Edits, Contact @Joelkb
-
-from aiohttp import web
-
-routes = web.RouteTableDef()
-
-@routes.get("/", allow_head=True)
-async def root_route_handler(request):
-    return web.json_response("Joelkb")
