048eea10ada8bea768366ed0cc4e12468ca9603f
CyberTG <123615887+CyberTG@users.noreply.github.com>
2023-01-30 20:45:31 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,15 +22,15 @@ async def autoapprove(c, m):
     try:
         await c.approve_chat_join_request(m.chat.id, m.from_user.id)
         button = [[
-            InlineKeyboardButton('🎬JOIN MOVIE CHANNEL🎬', url='https://t.me/+9JsGhjucwsozYjdl')
+            InlineKeyboardButton('🎬JOIN MOVIE CHANNEL🎬', url='https://t.me/at3moviesofficalbot')
             ],[
-            InlineKeyboardButton('💎JOIN MOVIE GROUP📽️', url='https://t.me/+iiJvauon_6Q1Njk1')
+            InlineKeyboardButton('📽️JOIN MOVIE GROUP📽️', url='https://t.me/MovieGroup_TM')
         ]]
         markup = InlineKeyboardMarkup(button)
         caption = f'Hello {m.from_user.mention()}\nYou Request To Join {m.chat.title} Was Approved.'
         await c.send_photo(
             m.from_user.id, 
-            photo='https://telegra.ph/file/2f8ce02ad5148d9e1dbd3.jpg', 
+            photo='https://telegra.ph/file/f7738f04ea74e16c9db02.jpg', 
             caption=caption, 
             reply_markup=markup
         )
@@ -48,12 +48,12 @@ async def start(c, m):
 I'm an auto approve Admin Join Requests Bot.
 I can approve users in Groups/Channels.Add me to your chat and promote me to admin with add members permission.'''
     button = [[
-            InlineKeyboardButton('💎JOIN CHANNEL✅️', url='https://t.me/+f16LP7YW2G03MzM1')
+            InlineKeyboardButton('💎JOIN GROUP✅️', url='https://t.me/MovieGroup_TM')
             ],[
-            InlineKeyboardButton('📣UPDATES🔔', url='https://t.me/+FCUs6o6FXSZkNjc6')
+            InlineKeyboardButton('📣UPDATES🔔', url='https://t.me/czdbotz')
     ]]
     await m.reply_photo(
-        photo='https://telegra.ph/file/7e184ab744d9aaa329a63.jpg', 
+        photo='https://telegra.ph/file/f7738f04ea74e16c9db02.jpg', 
         caption=text,
         reply_markup=InlineKeyboardMarkup(button),
         quote=True
